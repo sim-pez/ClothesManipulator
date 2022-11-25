0093ade5b671c59dcbfeef89d3440442dd708102
fatemah-alh <fatemah.mh@gmail.com>
2022-11-25 12:39:22 +0100

    modified output with feature

--- src/multi_manipulation_gen.py
+++ src/multi_manipulation_gen.py
@@ -5,51 +5,21 @@ import constants as C
 import random
 import torchvision.transforms as transforms
 import numpy as np
+import h5py
 from dataloader import Data
 from pprint import pprint
 
 from utils import split_labels, flatten_labels
 
-def multi_manipulation_gen(file_root, img_root_path, N):
+def multi_manipulation_gen(file_root, img_root_path, N, max_manip):
     '''
     Output files:
     couples.txt  ->  couples of Q and T that have the same non-zero attribute index (problably it is only for internal use)
     selected_couples_for_manip.txt  ->  couples of couples.txt which len(non-zero differente attributes) > N
     manipulations.txt  ->  generates manipulation vector from couples of selected_couples_for_manip.txt
-    '''
+    '''        
 
-    def find_couples(data):
-        '''
-        return couples of q,t that are compatible, i.e. that have the same all-zero attributes
-        '''
-
-        def check_match(q,t):
-
-            for attr_q, attr_t in zip(q,t):
-                if all(e == 0 for e in attr_t):
-                    if not all(f == 0 for f in attr_q):
-                        return False
-                if all(f == 0 for f in attr_q):
-                    if not all(e == 0 for e in attr_t):
-                        return False
-
-            return True
-
-
-        attr_num = data.attr_num
-        labels = data.label_data
-
-        splitted_labels = [split_labels(l, attr_num) for l in labels]
-        couples = [(i,j) for i, q in enumerate(tqdm(splitted_labels))
-                            for j, t in enumerate(splitted_labels)
-                                if i != j
-                                    if check_match(q,t)]
-
-
-        return couples
-
-
-    def create_n_manipulations(data, t_id, q_id, N):
+    def create_n_manipulations(q_labels, t_labels, N, attr_num):
         '''
         generate a multi-manipulation of N manipulations.
         Return None if number of candidate attributes are not enough to do random manipulations.
@@ -92,12 +62,7 @@ def multi_manipulation_gen(file_root, img_root_path, N):
 
             return manipulations_splitted
 
-        #initialization
-        q_labels = data.label_data[q_id]
-        t_labels = data.label_data[t_id]
-        attr_num = data.attr_num
         q_labels_splitted = split_labels(q_labels, attr_num)
-
 
         #extract needed manipulation to go from q to t
         manipulations = t_labels - q_labels
@@ -122,7 +87,49 @@ def multi_manipulation_gen(file_root, img_root_path, N):
 
         return multi_manipulation
     
+    def is_couple_match(q_labels, t_labels, N, attr_num):
 
+        def count_available_attributes(q_labels, manipulations, attr_num):
+
+            minus_manip = list(manipulations)
+            for i,e in enumerate(minus_manip):
+                if e == 1:
+                    minus_manip[i] = 0
+
+            avail_labels = q_labels + minus_manip
+            avail_labels_splitted = split_labels(avail_labels, attr_num)
+            
+            count = 0
+            for attr in avail_labels_splitted:
+                if 1 in attr:
+                    count += 1
+            
+            return count
+
+            
+        t_labels_splitted = split_labels(t_labels, attr_num)
+        q_labels_splitted = split_labels(q_labels, attr_num)
+
+
+        #check if zero attributes are the same
+        for attr_q, attr_t in zip(q_labels_splitted, t_labels_splitted):
+            if all(e == 0 for e in attr_t):
+                if not all(f == 0 for f in attr_q):
+                    return False
+            if all(f == 0 for f in attr_q):
+                if not all(e == 0 for e in attr_t):
+                    return False
+
+        #check if there are more available attributes than needed
+        manipulations = t_labels - q_labels
+        needed = max(np.count_nonzero(manipulations == 1), np.count_nonzero(manipulations == -1))
+        remaining = N - needed
+        if remaining > 0:
+            available_att = count_available_attributes(q_labels, manipulations, attr_num)
+            if available_att < remaining:
+                return False
+                
+        return True
     '''
     dataset for testing purpouses
 
@@ -130,7 +137,7 @@ def multi_manipulation_gen(file_root, img_root_path, N):
     #img_root_path = 'mini_ds/Images'
     
     '''
-
+    #loat attributes
     print('Loading attributes')
     train_data = Data(file_root,  img_root_path, 
                       transforms.Compose([
@@ -140,55 +147,90 @@ def multi_manipulation_gen(file_root, img_root_path, N):
                           transforms.ToTensor(),
                           transforms.Normalize(mean=C.IMAGE_MEAN, std=C.IMAGE_STD)
                       ]), 'train')
-    
-    #find couples
-    print("Finding couples of Q and T")
-    couples = find_couples(train_data)
-    random.shuffle(couples)
-
-    out = open(f"multi_manip/couple_manip_N_{N}.txt", "w")
-
-    for couple in tqdm(couples):
-        manipulations = create_n_manipulations(train_data, couple[0], couple[1], N)
-        if manipulations is not None:
-            manipulations_str = [str(m) for m in manipulations]
-            out.write(str(couple[0]) + "," + str(couple[1]) + "," + manipulations_str + "\n")
-    
-    out.close()
-
-def check_output(N):
     '''
-    checks if output is correctly saved
-    '''    
-    print("Checking output files...")
+    #validation set
+    valid_data = Data(file_root,  img_root_path,
+                  transforms.Compose([
+                      transforms.Resize((C.TARGET_IMAGE_SIZE, C.TARGET_IMAGE_SIZE)),
+                      transforms.ToTensor(),
+                      transforms.Normalize(mean=C.IMAGE_MEAN, std=C.IMAGE_STD)
+                  ]), 'test')
+    '''
+    
+    features_train = np.load('eval_out/feat_train.npy')
+    labels = train_data.label_data
+    attr_num = train_data.attr_num
 
-    f = open(f"multi_manip/couple_manip_N_{N}.txt")
+    print("Finding triplets")
+    triplets = []
+    t_shuffled_idx = np.array([i for i in range(len(labels))])
+        
+    for q_id in tqdm(range(len(labels))):
+        count = 0
+        q_labels = labels[q_id]
+        random.shuffle(t_shuffled_idx)
 
-    if len(f.readlines()) > 0:
-        print("Done! Check passed!")
-    else:
-        print("Check not passed")
+        for t_id in t_shuffled_idx:
+            t_labels = labels[t_id]
+            if q_id != t_id and is_couple_match(q_labels, t_labels, N, attr_num):
+                manipulations = create_n_manipulations(q_labels, t_labels, N, attr_num)
+                if manipulations is not None:
+                    triplets.append((features_train[q_id], features_train[t_id], manipulations))
+                    count += 1
+                    if count >= max_manip:
+                        break
 
-    f.close()
+    del train_data, features_train
 
+    #stringify triplets
+    print("writing triplets")
+    triplets_stringified = []
+    for triplet in triplets:
+        
+        triplet_str = ['_'.join(map(str, list)) for list in triplet]
+        triplet_str = '#'.join(triplet_str)
+        triplets_stringified.append(triplet_str)
 
-    out = open(f"multi_manip/couple_manip_N_{N}.txt", "w")
+    #save triplets
+    hf = h5py.File(f'multi_manip/train/triplets_N_{N}.h5', 'w')
+    hf.create_dataset('triplets', data=triplets_stringified)
+    hf.close()
 
-    i = 0
-    for couple_str in tqdm(g):
-        manip = e.readline()
-        cpl = (list(couple_str.strip().split(" ")))
+    print("done!")
+    '''
+    #to reopen file:
+    hf = h5py.File(f'multi_manip/train/triplets_N_{N}.h5', 'r')
+    ds = hf['triplets']
 
-        newline = cpl[0] + "," + cpl[1] + "," + manip.strip() + "\n"
-        out.write(newline)
+    def string2list(str_line):
+        str_triplet_list = str_line.decode("utf-8").split("#")
+        a = [float(e) for e in str_triplet_list[0].split("_")]
+        b = [float(e) for e in str_triplet_list[1].split("_")]
+        c = [int(e) for e in str_triplet_list[2].split("_")]
+        return [np.array(a, dtype='float32'), np.array(b, dtype='float32'), np.array(c)]
 
-    out.close() 
+    triplets = []
+    for line in ds:
+        triplet = string2list(line)
+        triplets.append(triplet)
+
+    hf.close()
+    '''
+    
+    #TODO fai sia train che test set
+
 
 if __name__ == '__main__':
     
-    N = 9
+    N = 8
+    max_manip = 3
     file_root = 'splits/Shopping100k'
     img_root_path = '/Users/simone/Desktop/VMR/Dataset/Shopping100k/Images'
 
-    multi_manipulation_gen(file_root, img_root_path, N)
-    check_output(N)
+    #dataset for testing purpouses
+    #file_root = 'mini_ds/splits/Shopping100k'
+    #img_root_path = 'mini_ds/Images'
+    
+    multi_manipulation_gen(file_root, img_root_path, N, max_manip) #TODO fai sia train che test
+
+    
