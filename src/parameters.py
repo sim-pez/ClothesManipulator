76f5a42f24b0f843b6c62cc6198b2724259a3852
fatemah-alh <fatemah.mh@gmail.com>
2023-01-11 13:49:24 +0100

    ..

--- src/parameters.py
+++ src/parameters.py
@@ -1,9 +1,15 @@
 #%%
 import os
+MODEL_EVAL="01-05-15:44"
 VAL_ORIGINAL=False
+NUM_LAYER=2
+N = 1
+NUM_EPOCH=20
+LR=0.001
+name_data_set="couples_N_1.h5"
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR=lambda x,y :os.path.join(y,"multi_manip/{}".format(x))
-DATA_IDS=lambda y: os.path.join(y,"couples_N_1.h5")
+DATA_IDS=lambda y: os.path.join(y,name_data_set)
 
 DATA_TRAIN_DIR=DATA_DIR("train",ROOT_DIR)
 DATA_TEST_DIR=DATA_DIR("test",ROOT_DIR)
@@ -22,11 +28,7 @@ FEAT_TRAIN_SENZA_N=os.path.join(ROOT_DIR,"eval_out/feat_train_senzaNorm.npy")
 FEAT_TEST_SENZA_N=os.path.join(ROOT_DIR,"eval_out/feat_test_senzaNorm.npy")
 LABEL_TRAIN=os.path.join(ROOT_DIR,"splits/Shopping100k/labels_train.txt")
 LABEL_TEST=os.path.join(ROOT_DIR,"splits/Shopping100k/labels_test.txt")
-NUM_LAYER=2
-N = 1
-NUM_EPOCH=20
-MODEL_EVAL="01-05-15:44"
-LR=0.001
+
 
 if __name__=="__main__":
     print(DATA_TRAIN,DATA_TEST)
